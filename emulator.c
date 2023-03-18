cfc26f0bc17b0e283bcadd6ce23a3596e4acbb06
bshree01 <brysonshreeve@gmail.com>
2023-03-18 18:43:43 -0400

    Working on reading ROM. Work in progress...

--- emulator.c
+++ emulator.c
@@ -2,6 +2,26 @@
 
 int main() 
 {
-   printf("Does this new update from VSCode get committed and pushed to Git?");
+   unsigned char memory[4096] = {0};
+   
+   //assign memory to be all zeros to see if rom is actually getting loaded
+
+   //Location of Game ROM
+   char file_name[] = "\\roms\\games\\Space Invaders [David Winter].ch8";
+   
+   //Open the ROM file ("rb" means read mode, binary)
+   FILE* rom_file = fopen(file_name, "rb");
+   
+   //Read the file
+   fread(memory,1,4096,rom_file);
+
+   for(int x = 0; x < 4096; x++)
+   {
+      printf("%u", memory[0]);
+   }
+   
+   // They are all still 0, so memory isn't getting assigned the values of rom_file properly
+
+   printf("Reached end of program");
    return 0;
 }
