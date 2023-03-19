62fd4ecd9afb4439a9285192e5471698cfcb9698
bshree01 <brysonshreeve@gmail.com>
2023-03-18 21:41:33 -0400

    Read every row and extract opcodes

--- emulator.c
+++ emulator.c
@@ -3,13 +3,15 @@
 #include <dirent.h> 
 #include <string.h>
 
-int main() 
-{
     //Declare memory array; initialize all to zero 
     unsigned char memory[4096] = {0};
-        
-    //0x200-0xFFF - Program ROM and work RAM
 
+    unsigned short opcode; //2 bytes
+    unsigned short counter;
+    unsigned short const counter_start = 0x200;
+
+int main() 
+{
     //Location of example Game ROM
     char file_name[] = ".\\roms\\games\\Space Invaders [David Winter].ch8";
 
@@ -23,25 +25,36 @@ int main()
         exit(-1); 
     }
 
-    //    //Seek to the memory region where ROM will be loaded
-    //    fseek(rom_file,0x200, SEEK_SET);
-
-    //    //Read the file
-    //    fread(memory + 0x200,1,4096-0x200,rom_file);
-
+    //Seek to counter_start (where the ROM load needs to begin)
+    fseek(rom_file, counter_start, SEEK_SET);
 
     //Read the file
-    fread(memory,1,4096,rom_file);
+    fread(memory + counter_start, 1, 4096 - counter_start, rom_file);
 
     //Close the ROM file
     fclose(rom_file);
 
-    for(int x = 0; x < 4096; x++)
+    //Initialize counter to ROM starting location (0x200)
+    counter = counter_start;
+        
+    //0x200-0xFFF - Program ROM and work RAM
+
+    //Temporary: look at every opcode in order
+    for(int x = 0; x < 4096; x += 2)
     {
-        printf("%u", memory[x]);
+        //opcode = two bytes at the program counter
+        opcode = memory[counter] << 8 | memory[counter + 1];
+
+        //Move program counter + 2
+        counter += 2;
+
+        //Print all non-zero opcodes
+        if (opcode != 0)
+        {
+            printf("opcode%i: %x\n", 1+x/2, opcode);
+        }
     }
 
-    // They are all still 0, so memory isn't getting assigned the values of rom_file properly
 
     printf("Reached end of program");
     return 0;
